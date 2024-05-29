dc233d30f3248acdbfd8de80e9bd01ac4ae5e81a
Venkatesan-1610 <vickyvenkattnr123@gmail.com>
2024-05-29 19:53:05 +0530

    Added Etityframework modified files

---
+++ ConnectionStringProvider .cs
@@ -0,0 +1,14 @@
+﻿using EmployeeDetails.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDetails.Services
+{
+    public class ConnectionStringProvider : DbContext
+    {
+        public ConnectionStringProvider(DbContextOptions<ConnectionStringProvider> options) : base(options)
+        {
+        }
+
+        public DbSet<EmployeeDetailsRequest> Contacts { get; set; }
+    }
+}
---
+++ ConnectionStringProviderFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace EmployeeDetails.Services
+{
+    public class ConnectionStringProviderFactory : IDesignTimeDbContextFactory<ConnectionStringProvider>
+    {
+        public ConnectionStringProvider CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ConnectionStringProvider>();
+
+            // Read the configuration from appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ConnectionStringProvider(optionsBuilder.Options);
+        }
+    }
+}
